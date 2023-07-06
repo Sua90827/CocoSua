b16c777031f76e8500f1562abef0a01941361c2a
Sua90827 <kimsuasch@gmail.com>
2023-07-06 20:17:55 +0900

    commit here

---
+++ project/src/main/java/com/suamall/project/configuration/MemberService.java
@@ -0,0 +1,43 @@
+package com.suamall.project.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.suamall.project.dto.MemberDTO;
+import com.suamall.project.repository.MemberRepository;
+
+import java.util.Collections;
+
+
+@Service("configService")
+public class MemberService implements UserDetailsService {
+    @Autowired
+    private MemberRepository repo;
+
+    public MemberDTO findId(String username) throws Exception {
+        return repo.idCheck(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        MemberDTO member = null;
+        try {
+            member = repo.idCheck(username);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        if(member == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return User.builder()
+                .username(member.getMember_id())
+                .password(member.getMember_pw())
+                .authorities(Collections.emptyList())
+                .build();
+    }
+}
---
+++ project/src/main/java/com/suamall/project/dto/ProductDTO.java
@@ -0,0 +1,11 @@
+package com.suamall.project.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductDTO {
+	private int cate_id;
+	private String cate_nm;
+}
