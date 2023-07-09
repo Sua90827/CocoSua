1e5ec69cceac7a48a321897ee758e8f5bfdaacac
Sua90827 <kimsuasch@gmail.com>
2023-07-09 16:32:04 +0900

    dehedehe

--- project/src/main/java/com/suamall/project/repository/ProductRepository.java
+++ project/src/main/java/com/suamall/project/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.suamall.project.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.suamall.project.dto.CategoryDTO;
+import com.suamall.project.dto.ColorDTO;
 
 @Mapper
 public interface ProductRepository {
@@ -10,5 +13,14 @@ public interface ProductRepository {
 	CategoryDTO checkDB(CategoryDTO input);
 
 	void cate_id(CategoryDTO input);
+
+	List<ColorDTO> getColorList();
+
+	List<CategoryDTO> getCategoryList();
+	
+//	ColorDTO dto = new ColorDTO();
+//	dto.setColor_id(1),
+//	dto.setColor_nm("sjiod");
+//	list.add(dto)
 
 }
--- project/src/main/java/com/suamall/project/service/ProductService.java
+++ project/src/main/java/com/suamall/project/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.suamall.project.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.suamall.project.dto.CategoryDTO;
+import com.suamall.project.dto.ColorDTO;
 import com.suamall.project.repository.ProductRepository;
 
 @Service
@@ -17,10 +20,22 @@ public class ProductService {
 		}		
 		CategoryDTO db = new CategoryDTO();
 		db=repo.checkDB(input);
-		if(db.getCate_nm()!=null) {
+		if(db!=null) {
 			return "이미 존재하는 카테고리입니다.";
 		}
 		repo.cate_id(input);
+		return null;
+	}
+
+	public List<ColorDTO> getColorList() {
+		
+		List<ColorDTO> color = repo.getColorList();
+		
+		return color;
+	}
+
+	public List<CategoryDTO> getCategoryList() {
+		List<CategoryDTO> cate = repo.getCategoryList();
 		return null;
 	}
 }
